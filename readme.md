eb6613e93e4ab51813c5ec1ae9967830aa5aa38f
Alan Mól <alan3df@gmail.com>
2016-12-11 01:03:22 -0200

    Adding GOtools to extract lists of GOs

--- readme.md
+++ readme.md
@@ -3,6 +3,10 @@
 ## LBQPtools
 
 
-This Shiny App consists of a collection of tools that have been created to solve daily problems we have encountered in our lab. The idea of using Shiny is that we can take advantage of all R's powerful features for the most different tasks, as well as easily share them with cowokers and other researchers.
+This Shiny App consists of a collection of tools that have been created to solve daily problems we have encountered in [our lab](http://lbqp.unb.br/). The idea of using Shiny is that we can take advantage of all [R Language](https://www.r-project.org/)'s powerful features for the most different tasks, as well as easily share them with cowokers and other researchers.
 
-This tool also links to NetWheels, a separate Shiny App that is also hosted here on Github. 
+The app is available at http://lbqp.unb.br/LBQPtools/
+
+This tool also links to [NetWheels](http://lbqp.unb.br/NetWheels/), a separate Shiny App that is also hosted here on Github. 
+
+These tools have been developed by Alan R. Mól under the supervision of Prof. Wagner Fontes, with the occasional help and input from several colleagues. 
--- server.R
+++ server.R
@@ -4,16 +4,17 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  inFile1 <- reactive(input$file1)
+  ##### FASTA TOOLS #####
   
-  inFile2 <- reactive(input$file2)
+  #inFile1 <- reactive(input$file_Fasta1)
+  #inFile2 <- reactive(input$file_Fasta2)
   
-  Fasta1 <- reactive(seqinr::read.fasta(input$file1$datapath, seqtype = input$seqtype,
+  Fasta1 <- reactive(seqinr::read.fasta(input$file_Fasta1$datapath, seqtype = input$seqtype,
                                         as.string = TRUE))
   
   output$hp_NumOfSeq <- renderUI({
     
-    if (is.null(inFile1())) {
+    if (is.null(input$file_Fasta1)) {
       helpText("Number of sequences:", style = "font-weight: bold; color: #000000;")
     } else {
       sequences <- Fasta1()
@@ -31,32 +32,32 @@ shinyServer(function(input, output) {
       #f1 <- seqinr::read.fasta("WF1.fasta", seqtype = "AA",
       #as.string = TRUE)
       
-      f2 <- seqinr::read.fasta(inFile2()$datapath, seqtype = input$seqtype,
+      f2 <- seqinr::read.fasta(input$file_Fasta2$datapath, seqtype = input$seqtype,
                                as.string = TRUE)
       #f2 <- seqinr::read.fasta("WF2.fasta", seqtype = "AA",
       #as.string = TRUE)
       
       headers <- clearHeader(seqinr::getAnnot(c(f1, f2)))
       
-      dups <- if (input$cb_RemoveRedundancies) {
+      dups <- if (input$cb_rmFastaDups) {
         duplicated(c(as.character(f1), as.character(f2)))
       } else {
         FALSE
       }
       
-      revs <- if (input$cb_RemoveReverses) {
+      revs <- if (input$cb_rmFastaRevs) {
         grepl("REVERSED", headers)
       } else {
         FALSE
       }
       
-      conts <- if (input$cb_RemoveContaminants) {
+      conts <- if (input$cb_rmFastaConts) {
         grepl("CONTAMINANT", headers)
       } else {
         FALSE
       }
       
-      tagsr <- if (input$cb_RemoveTag && (input$tx_tagRemove != "")) {
+      tagsr <- if (input$cb_rmFastaTag && (input$tx_tagRemove != "")) {
         grepl(input$tx_tagRemove, headers, fixed = TRUE)
       } else {
         FALSE
@@ -76,8 +77,8 @@ shinyServer(function(input, output) {
       "FilteredFasta.fasta"
     },
     content = function(file) {
-      fastaIn <- seqinr::read.fasta(inFile1()$datapath, seqtype = input$seqtype,
-                               as.string = TRUE)
+      fastaIn <- Fasta1()#seqinr::read.fasta(inFile1()$datapath, seqtype = input$seqtype,
+                         #      as.string = TRUE)
       
       # fastaIn <- seqinr::read.fasta("2seqFasta.fasta", seqtype = "AA",
       #                               as.string = TRUE)
@@ -96,6 +97,79 @@ shinyServer(function(input, output) {
   observeEvent(input$bt_resetHeaderList, {
     reset("fileHeaderFilter")
   })
+  
+  
+  
+  ##### GO TOOLS #####
+  
+  b2gTable <- reactive(readr::read_delim(input$file_b2gTable$datapath, delim = "\t", 
+                                  escape_double = FALSE, trim_ws = TRUE))
+  
+  output$hp_NumGOLines <- renderUI({
+    if (is.null(input$file_b2gTable)) {
+      helpText("Number of lines:", style = "font-weight: bold; color: #000000;")
+    } else {
+      tab <- b2gTable()
+      helpText(paste("Number of lines:", nrow(tab)), style = "font-weight: bold; color: #000000;")
+    }
+  })
+  
+  output$bt_extractGO_all <-  downloadHandler(
+    filename = function() {
+      "GO_all_list.txt"
+    },
+    content = function(file) {
+      out <- extract.go(GOvec = b2gTable()$`GO IDs list`,
+                        type = "all", removeCat = input$cb_rmGOcat,
+                        removeDups = input$cb_rmGOdups)$vec
+      
+      writeLines(out, con = file)
+    })
+  
+  output$bt_extractGO_C <-  downloadHandler(
+    filename = function() {
+      "GO_C_list.txt"
+    },
+    content = function(file) {
+      out <- extract.go(GOvec = b2gTable()$`GO IDs list`,
+                        type = "C", removeCat = input$cb_rmGOcat,
+                        removeDups = input$cb_rmGOdups)$vec
+      
+      writeLines(out, con = file)
+    })
+  
+  output$bt_extractGO_F <-  downloadHandler(
+    filename = function() {
+      "GO_F_list.txt"
+    },
+    content = function(file) {
+      out <- extract.go(GOvec = b2gTable()$`GO IDs list`,
+                        type = "F", removeCat = input$cb_rmGOcat,
+                        removeDups = input$cb_rmGOdups)$vec
+      
+      writeLines(out, con = file)
+    })
+  
+  output$bt_extractGO_P <-  downloadHandler(
+    filename = function() {
+      "GO_P_list.txt"
+    },
+    content = function(file) {
+      out <- extract.go(GOvec = b2gTable()$`GO IDs list`,
+                        type = "P", removeCat = input$cb_rmGOcat,
+                        removeDups = input$cb_rmGOdups)$vec
+      
+      writeLines(out, con = file)
+    })
+  
+  output$plot_GOpie <- renderPlot({
+    cats <- extract.go(GOvec = b2gTable()$`GO IDs list`,
+                               type = "all", removeCat = input$cb_rmGOcat,
+                               removeDups = input$cb_rmGOdups)$cats
+    
+    pie(table(cats))
+  })
+  ##### iTraq Ratio Check #####
   
 })
 
@@ -137,6 +211,26 @@ filter.fasta <- function(sequences, headers, minRes, maxRes) {
   return(filtered)
 }
 
+extract.go <- function(GOvec, type = c("all", "C", "F", "P"),
+                       removeCat = TRUE, removeDups = FALSE) {
+  vec <- unlist(strsplit(na.omit(GOvec), split = "; "))
+  
+  if (removeDups) {
+    vec <- unique(vec)
+  }
+  
+  cats <- gsub(":GO:\\d{7}", "", vec)
+  
+  if (removeCat) {
+    vec <- gsub("[CFP]:", "", vec)
+  }
+  
+  if (type[1] != "all") {
+    vec <- vec[cats == type[1]]
+  }
+  
+  list(vec = vec, cats = cats)
+}
 
 
 
